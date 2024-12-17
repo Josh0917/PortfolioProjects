a6dbc6896d8b69c06049f4aeac371209875e3f53
josh0917 <joshuajordan0917@gmail.com>
2024-12-16 21:20:15 -0600

    Starting a new Battleship program from scratch to get better at arrays, functions, and I/O.

---
+++ C_Programs/Battleship.c
@@ -0,0 +1,34 @@
+/*
+  Synopsis: Playing Battleship
+  Author: Josh J
+  Date:
+*/
+#include <stdio.h>
+#define X 5
+#define Y 5
+
+
+//Function prototype
+char printGrid(char row[X][Y], size_t one, size_t two);
+
+
+//main
+int main(void){
+
+char row[X][Y] =
+{
+ {'-','-','-','-','-'}
+ {'-','-','-','-','-'}
+ {'-','-','-','-','-'}
+ {'-','-','-','-','-'}
+ {'-','-','-','-','-'}
+};
+
+
+return 0;
+}
+
+//Functions
+char printGrid(char row[X][Y], size_t one, size_t two){
+
+}
