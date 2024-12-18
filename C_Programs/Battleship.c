00fca974a9840c4e2ae65953d5bfabd6a3854118
josh0917 <joshuajordan0917@gmail.com>
2024-12-17 22:05:41 -0600

    Correction to the Grid generation and the associated output file.

--- C_Programs/Battleship.c
+++ C_Programs/Battleship.c
@@ -15,20 +15,34 @@ char printGrid(char row[X][Y], size_t one, size_t two);
 //main
 int main(void){
 
+//Variables
+char guess[X][Y];
+
+
 char row[X][Y] =
 {
- {'-','-','-','-','-'}
- {'-','-','-','-','-'}
- {'-','-','-','-','-'}
- {'-','-','-','-','-'}
+ {'-','-','-','-','-'},
+ {'-','-','-','-','-'},
+ {'-','-','-','-','-'},
+ {'-','-','-','-','-'},
  {'-','-','-','-','-'}
 };
 
+printGrid(row, X, Y);
+
+printf("Player One place your ships");
+
+//scanf("",);
 
 return 0;
 }
 
 //Functions
 char printGrid(char row[X][Y], size_t one, size_t two){
-
+  for(int i = 0; i < one; i++){
+    for(int j = 0; j < two; j++){
+      printf("-");
+    }
+    printf("\n");
+  }
 }
